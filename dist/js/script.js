92044d81eeea6afca9e600bb2fad9ab9c9c7174b
tomohatsuNotePC <chronos1986@outlook.com>
2023-08-01 00:22:47 +0900

    20230731-pm

--- dist/js/script.js
+++ dist/js/script.js
@@ -45,3 +45,46 @@ jQuery(function ($) {
   });
 
 });
+
+$(document).ready(function () {
+
+  const $submitBtn = $('#js-submit')
+  $('#form input,#form textarea').on('change', function () {
+    if (
+      $('#form #name').val() !== "" &&
+      $('#form #mail').val() !== "" &&
+      $('#form #content').val() !== ""
+    ) {
+      $submitBtn.prop('disabled', false);
+
+    } else {
+      $submitBtn.prop('disabled', true);
+    }
+  });
+
+});
+
+$(document).ready(function () {
+
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSelYLlpZuJ2riYHLcmKwt7SK7ViXXaSpZaVu34sqkDWDaAKIg/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".p-contact__end-message").slideDown();
+          $(".p-contact__submit").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".p-contact__false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
